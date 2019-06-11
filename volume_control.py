55ac4865119570c80a176c79eb9e304d886e3f1f
apospraf <apospraf1@hotmail.com>
2019-06-12 00:39:22 +0300

    Added mute button

--- volume_control.py
+++ volume_control.py
@@ -9,20 +9,24 @@ interface = devices.Activate(
    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 c = 0
-
-# 
-
-
+mute = False
 while True:
 	currentVolume = volume.GetMasterVolumeLevel()
 	line = se.readline().strip()
 	if len(line)>0:
 		c = c + 1
 		values = line.decode('ascii').split(',')
-		newVolume = round(int(values[0])*60/1023)
-		if abs(newVolume-currentVolume)> 10:
-			if c>5:
-				volume.SetMasterVolumeLevel(-newVolume, None)
-				c=0
+		x = int(values[0])
+		print(x)
+		if x>0 and not mute:
+			newVolume = round(x*60/1023)
+			if abs(newVolume-currentVolume)> 10:
+				if c>5:
+					volume.SetMasterVolumeLevel(-newVolume, None)
+					c=0
+		elif x==-1:
+			volume.SetMute(not mute, None)
+			mute = not mute
+			
 		
 		
